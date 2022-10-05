01d28923a0de8035639080da8c633fed3674fea9
= <=>
2022-10-05 11:26:14 +0200

    dbconnection

---
+++ dbconnection.php
@@ -0,0 +1,23 @@
+<?php
+
+
+function connect() {
+    $host = '127.0.0.1';
+    $db   = 'dejavu.';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+        return $pdo;
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+} //end function connect
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+require_once("dbconnection.php");
+$pdo = connect();
+
 include_once("header.php");
 
 echo ("Hier komt de body content");
