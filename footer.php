2c90d402a22c460dc6a9c34d07d4d0ddc7294e14
= <=>
2022-10-05 11:37:30 +0200

    footer

--- footer.php
+++ footer.php
@@ -7,10 +7,10 @@
         <p> &copy; <?php echo date("Y"); ?>
             DejaVu </P>
         <p> Gemaakt door
-            <a class="mail_link" href="mailto:test@vistacollege.nl">Robin Reinders,</a>
-            <a class="mail_link" href="mailto:test@vistacollege.nl">Vigo lutke schipholt,</a>
-            <a class="mail_link" href="mailto:test@vistacollege.nl">Paris Stassen</a> en
-            <a class="mail_link" href="mailto:test@vistacollege.nl">Dennis van Drongelen</a>
+            <a class="mail_link" href="mailto:212375@vistacollege.nl">Robin Reinders,</a>
+            <a class="mail_link" href="mailto:508346@vistacollege.nl">Vigo lutke schipholt,</a>
+            <a class="mail_link" href="mailto:505825@vistacollege.nl">Paris Stassen</a> en
+            <a class="mail_link" href="mailto:508089@vistacollege.nl">Dennis van Drongelen</a>
         </p>
     </div>
 </body>
