c56d269c36105cb50ee2b46eca60821d228a8729
Parisxx <505825@vistacollege.nl>
2022-10-05 10:07:12 +0200

    footer en header

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 <head>
-  <link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="styles.css">
 </head>
+<body>
+    <p> Hier komt de footer </p>
+</body>
 
 <?php
 
 echo "Dit is de footer";
+
--- header.php
+++ header.php
@@ -1,8 +1,14 @@
 <head>
-  <link rel="stylesheet" href="styles.css">
-  <link rel="icon" href="media/icon.png" type="image/png" />
+<link rel="stylesheet" href="styles.css">
+<link rel="icon" href="media/icon.png" type="image/png" />
 </head>
+<body>
+    <p> Hier komt de header </p>
+</body>
 
 <?php
 
 echo "Dit is de header";
+
+
+
