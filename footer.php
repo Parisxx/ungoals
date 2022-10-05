0b38aff37667df92dceeeec23dec71bb2db40c55
Parisxx <505825@vistacollege.nl>
2022-10-05 10:02:56 +0200

    Links

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+<head>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
 <?php
 
 echo "Dit is de footer";
--- header.php
+++ header.php
@@ -1,3 +1,8 @@
+<head>
+  <link rel="stylesheet" href="styles.css">
+  <link rel="icon" href="media/icon.png" type="image/png" />
+</head>
+
 <?php
 
 echo "Dit is de header";
