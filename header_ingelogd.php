c3af176f2fa3d55baa90b911d1c0a8895e4759ca
Parisxx <505825@vistacollege.nl>
2022-10-05 13:21:35 +0200

    paginas

---
+++ header_ingelogd.php
@@ -0,0 +1,24 @@
+<!-- HEADER START -->
+
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="media/logo.png" type="image/png" />
+</head>
+
+<body>
+    <div class="header">
+        <a href="index.php">
+            <img class="logo" src="media/logo.png" width="150" height="150">
+        </a>
+        <div class="button_header">
+            <a class="button" href="index.php">Home</a>
+            <a class="button" href="account.php">account</a>
+            <a class="button" href="checkout.php">checkout</a>
+            <a class="button" href="loguit.php">loguit</a>
+        </div>
+    </div>
+
+
+</body>
+
+<!-- HEADER END -->
