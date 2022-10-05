a568bb951eee6e2598ef733b2d54b14709b63707
Parisxx <505825@vistacollege.nl>
2022-10-05 13:09:55 +0200

    header and footer

--- header.php
+++ header.php
@@ -8,16 +8,16 @@
 <body>
     <div class="header">
         <a href="index.php">
-            <img class= "logo"src="media/logo.png" width="150" height="150">
+            <img class="logo" src="media/logo.png" width="150" height="150">
         </a>
         <div class="button_header">
-    <a class="button" href="index.php">Home</a>
-    <a class="button" href="inlog.php">inloggen</a>
-    <a class="button" href="checkout.php">checkout</a>
-</div>
+            <a class="button" href="index.php">Home</a>
+            <a class="button" href="inlog.php">inloggen</a>
+            <a class="button" href="checkout.php">checkout</a>
+        </div>
     </div>
 
-    
+
 </body>
 
 <!-- HEADER END -->
