2c7573df5df9911bc9ce98f8e6ab82dad098861e
Parisxx <505825@vistacollege.nl>
2022-10-05 13:37:20 +0200

    index

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 session_start();
 include("db_connect.php");
 $pdo = connect();
+$stmt = $pdo->query("SELECT * FROM product");
 
 include_once("header.php");
+
+while ($data = $stmt->fetch())
+{
+  echo "<div class='wrapper_product'>";
+  echo "<p class='product'>" . $data['name'] . "</p>";
+  echo "<p class='product'>" . $data['price'] . "</p>";
+  echo "<p class='product'>" . $data['location'] . "</p>";
+  echo "</div>";
+}
 
 echo ("Hier komt de body content");
 
