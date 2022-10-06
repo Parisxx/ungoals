7bef55abf360992084dec441f8b07f92eacb380f
Parisxx <505825@vistacollege.nl>
2022-10-06 10:31:35 +0200

    change

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@ include("includes/db_connect.php");
 $pdo = connect();
 $stmt = $pdo->query("SELECT * FROM product");
 
-include_once("includes/header.php");
+include_once("includes/header_ingelogd.php");
 
 
 include_once("includes/footer.php");
