dfe01468ef5d7f96d363a96d2454b435a068ca99
Parisxx <505825@vistacollege.nl>
2022-10-10 12:44:48 +0200

    index

--- index_ingelogd.php
+++ index_ingelogd.php
@@ -4,8 +4,7 @@ include("includes/db_connect.php");
 $pdo = connect();
 $stmt = $pdo->query("SELECT * FROM product");
 
-include_once("includes/header_ingelogd.php");
-
+include_once("includes/header.php");
 
 while ($data = $stmt->fetch())
 {
@@ -14,7 +13,11 @@ while ($data = $stmt->fetch())
   echo "<h3 class='product'>€" . $data['price'] . "</h3>";
   echo "<p class='product'>" . $data['location'] . "</p>";
   echo "<img src='uploads/" . $data['picture'] ."' width='100' height='100'>";
+  echo "<br>";
+  echo "<br>";
+  echo "<button> Add to cart </button>";
   echo "</div>";
 }
+
 
 include_once("includes/footer.php");
