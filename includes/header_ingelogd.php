3eca88c314c9a7e6a94ee6f7e8f712bcd7cbce87
Parisxx <505825@vistacollege.nl>
2022-10-10 12:46:06 +0200

    index

--- includes/header_ingelogd.php
+++ includes/header_ingelogd.php
@@ -11,7 +11,7 @@
             <img class="logo" src="media/logo.png" width="150" height="150">
         </a>
         <div class="button_header">
-            <a class="button" href="index.php">Home</a>
+            <a class="button" href="index_ingelogd.php">Home</a>
             <a class="button" href="account.php">account</a>
             <a class="button" href="checkout.php">checkout</a>
             <a class="button" href="loguit.php">loguit</a>
