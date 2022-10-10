370f3955bbf37a1c2f5d839cb4bfa12cfe09f7f0
Parisxx <505825@vistacollege.nl>
2022-10-10 13:27:31 +0200

    checkout

--- checkout.php
+++ checkout.php
@@ -9,12 +9,10 @@ include_once("includes/header_ingelogd.php");
 
 while ($data = $stmt->fetch())
 {
-  echo "<div class='product_wrapper'>";
-  echo "<h1 class='product'>" . $data['name'] . "</h1>";
-  echo "<h3 class='product'>€" . $data['price'] . "</h3>";
-  echo "<p class='product'>" . $data['location'] . "</p>";
-  echo "<img src='uploads/" . $data['picture'] ."' width='100' height='100'>";
-  echo "</div>";
+
+
+
+  
 }
 
 include_once("includes/footer.php");
--- index_ingelogd.php
+++ index_ingelogd.php
@@ -4,7 +4,9 @@ include("includes/db_connect.php");
 $pdo = connect();
 $stmt = $pdo->query("SELECT * FROM product");
 
-include_once("includes/header.php");
+include_once("includes/header_ingelogd.php");
+
+
 
 while ($data = $stmt->fetch())
 {
@@ -13,11 +15,33 @@ while ($data = $stmt->fetch())
   echo "<h3 class='product'>€" . $data['price'] . "</h3>";
   echo "<p class='product'>" . $data['location'] . "</p>";
   echo "<img src='uploads/" . $data['picture'] ."' width='100' height='100'>";
-  echo "<br>";
-  echo "<br>";
-  echo "<button> Add to cart </button>";
+
+  echo "<form action='index_ingelogd.php' method='post'>";
+  echo "<button  value='" . $data['id'] . "' name='submit' type='submit'>Add to cart</button>";
+  echo "</form>";
+
+  echo "<form action='index_ingelogd.php' method='post'>";
+  echo "<button  value='" . $data['id'] . "' name='submit' type='submit'>Add to cart</button>";
+  echo "</form>";
+
   echo "</div>";
 }
+
+
+    if(isset($_POST['submit']))
+    {
+        $user_id= $_POST['user_id'];
+        $product_id = $_POST['product_id'];
+
+        $sql = "INSERT INTO checkout (user_id, product_id) VALUES ('$user_id','$product_id')";
+        $result = $pdo->query($sql);
+        
+  
+    } 
+  
+    // if add to cart then send to checkout tabel when bought product id deleted from tabel product
+
+
 
 
 include_once("includes/footer.php");
