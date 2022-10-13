a387243f9577823e230225daa4757ea0f1d68070
Parisxx <505825@vistacollege.nl>
2022-10-13 09:37:45 +0200

    register

--- account.php
+++ account.php
@@ -6,7 +6,20 @@ $stmt = $pdo->query("SELECT * FROM user");
 
 include_once("includes/header_ingelogd.php");
 
+$email = $_SESSION["user"];
+$sql = "SELECT * FROM login WHERE email = '" .$email."'";
+$result = $conn->query($sql);
 
+while($row = $result->fetch(PDO::FETCH_ASSOC)){
+    $email = $row['email'];
+    $first = $row['firstname'];
+    $last = $row['lastname'];
+
+    echo "<p id='email1'>Your email is:</p>";
+    echo "<p id='email'>$email</p>";
+    echo "<p id='name1'>Your Full Name is:</p>";
+    echo "<p id='name'>$first $last</p>";
+}
 
 
 include_once("includes/footer.php");
--- includes/functions.php
+++ includes/functions.php
@@ -1,53 +1,53 @@
 <?php
-session_start();
+
 include("includes/db_connect.php");
 $pdo = connect();
 
-function adduser($conn){
-    // if(isset($_POST['submit'])){
-    //     $email = $_POST['e-mail'];
-    //     $firstname = $_POST['firstname'];
-    //     $lastname = $_POST['lastname'];
-    //     $password = $_POST['password'];
-    //     $confirm = $_POST['confirm'];
+// function adduser($conn){
+//     if(isset($_POST['submit'])){
+//         $email = $_POST['e-mail'];
+//         $firstname = $_POST['firstname'];
+//         $lastname = $_POST['lastname'];
+//         $password = $_POST['password'];
+//         $confirm = $_POST['confirm'];
 
-    //     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    //         header("location: registratie.php?error=invalidemail");
-    //         exit();
-    //     }else if(!preg_match("/^[a-zA-Z]*$/", $firstname)){
-    //         header("location: registratie.php?error=invalidfname");
-    //         exit();
-    //     }else if(!preg_match("/^[a-zA-Z]*$/", $lastname)){
-    //         header("location: registratie.php?error=invalidlname");
-    //         exit();
-    //     }
-    //     else if(!preg_match("/^[a-zA-Z0-9]*$/", $password)){
-    //         header("location: registratie.php?error=invalidpass");
-    //         exit();
-    //     }else if(!$password == $confirm){
-    //         header("location: registratie.php?error=pwdontmatch");
-    //     } else {
-    //         $sql = "SELECT * FROM login WHERE email = :email";
-    //         $stmt = $conn->prepare($sql);
-    //         $stmt->execute(['email' => $email]);
-    //         if($stmt->rowCount() !== 0){
-    //             header("location: registratie.php?error=userexists");
-    //             exit();
-    //         } else {
-    //             $query = "INSERT INTO login (email, firstname, lastname, password) VALUES(:email, :firstname, :lastname, :password)";
-    //             $stmt = $conn->prepare($query);
-    //             if(!$stmt){
-    //                 header("location: registratie.php?error=stmtfailed");
-    //                 exit();
-    //             } else {
-    //                 $hashedpwd = password_hash($password, PASSWORD_DEFAULT);
-    //                 $stmt->execute(['email' => $email, 'firstname' => $firstname, 'lastname' => $lastname, 'password' => $hashedpwd]);
-    //                 header("location: registratie.php?add=user");
-    //                 }
-    //             }
-    //             }
-    //         }
-        }
+//         if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+//             header("location: registratie.php?error=invalidemail");
+//             exit();
+//         }else if(!preg_match("/^[a-zA-Z]*$/", $firstname)){
+//             header("location: registratie.php?error=invalidfname");
+//             exit();
+//         }else if(!preg_match("/^[a-zA-Z]*$/", $lastname)){
+//             header("location: registratie.php?error=invalidlname");
+//             exit();
+//         }
+//         else if(!preg_match("/^[a-zA-Z0-9]*$/", $password)){
+//             header("location: registratie.php?error=invalidpass");
+//             exit();
+//         }else if(!$password == $confirm){
+//             header("location: registratie.php?error=pwdontmatch");
+//         } else {
+//             $sql = "SELECT * FROM login WHERE email = :email";
+//             $stmt = $conn->prepare($sql);
+//             $stmt->execute(['email' => $email]);
+//             if($stmt->rowCount() !== 0){
+//                 header("location: registratie.php?error=userexists");
+//                 exit();
+//             } else {
+//                 $query = "INSERT INTO login (email, firstname, lastname, password) VALUES(:email, :firstname, :lastname, :password)";
+//                 $stmt = $conn->prepare($query);
+//                 if(!$stmt){
+//                     header("location: registratie.php?error=stmtfailed");
+//                     exit();
+//                 } else {
+//                     $hashedpwd = password_hash($password, PASSWORD_DEFAULT);
+//                     $stmt->execute(['email' => $email, 'firstname' => $firstname, 'lastname' => $lastname, 'password' => $hashedpwd]);
+//                     header("location: registratie.php?add=user");
+//                     }
+//                 }
+//                 }
+//             }
+//         }
 
 function add(){
     if(isset($_GET['add'])){
@@ -96,20 +96,20 @@ function login_user($conn){
     if(isset($_POST['submit'])) {
         $email = $_POST['e-mail'];
         $password = $_POST['password'];
-        $sql = "SELECT * FROM login WHERE email = '" . $email . "'";
+        $sql = "SELECT * FROM user WHERE email = '" . $email . "'";
 
-        $stmt = $conn->query($sql);
+        $stmt = $pdo->query($sql);
         $user = $stmt->fetch();
 
         if ($user == 0) {
-            header("Location: inloggen.php?error=wrong_email");
+            header("Location: inlog.php?error=wrong_email");
             exit();
         } else {
             $pwdHashed = $user["password"];
             $checkpwd = password_verify($password, $pwdHashed);
 
             if ($checkpwd === false) {
-                header("Location: inloggen.php?error=wrong_pass");
+                header("Location: inlog.php?error=wrong_pass");
                 exit();
             } else {
                 session_start();
@@ -119,7 +119,7 @@ function login_user($conn){
                     exit();
                 } else {
                     $_SESSION["user"] = $user['email'];
-                    header("Location: login/login_index.php");
+                    header("Location: index_ingelogd.php");
                     exit();
                 }
             }
@@ -128,43 +128,43 @@ function login_user($conn){
 }
 
 function account($conn){
-    // $email = $_SESSION["user"];
-    // $sql = "SELECT * FROM login WHERE email = '" .$email."'";
-    // $result = $conn->query($sql);
+    $email = $_SESSION["user"];
+    $sql = "SELECT * FROM login WHERE email = '" .$email."'";
+    $result = $conn->query($sql);
 
-    // while($row = $result->fetch(PDO::FETCH_ASSOC)){
-    //     $email = $row['email'];
-    //     $first = $row['firstname'];
-    //     $last = $row['lastname'];
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        $email = $row['email'];
+        $first = $row['firstname'];
+        $last = $row['lastname'];
 
-    //     echo "<p id='email1'>Your email is:</p>";
-    //     echo "<p id='email'>$email</p>";
-    //     echo "<p id='name1'>Your Full Name is:</p>";
-    //     echo "<p id='name'>$first $last</p>";
-    // }
+        echo "<p id='email1'>Your email is:</p>";
+        echo "<p id='email'>$email</p>";
+        echo "<p id='name1'>Your Full Name is:</p>";
+        echo "<p id='name'>$first $last</p>";
+    }
 }
 
 function show_all($conn){
-    // $sql = "SELECT * FROM login";
-    // $result = $conn->query($sql);
-    // while($row = $result->fetch(PDO::FETCH_ASSOC)){
-    //     $email = $row['email'];
-    //     $first = $row['firstname'];
-    //     $last = $row['lastname'];
-    //     $user_type = $row['user_type'];
-    //     $time = $row['time'];
-    //     $date = $row['date'];
+    $sql = "SELECT * FROM login";
+    $result = $conn->query($sql);
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        $email = $row['email'];
+        $first = $row['firstname'];
+        $last = $row['lastname'];
+        $user_type = $row['user_type'];
+        $time = $row['time'];
+        $date = $row['date'];
 
-    //     echo "<tr>";
-    //     echo "<td>$email</td>";
-    //     echo "<td>$first $last</td>";
-    //     echo "<td>$time</td>";
-    //     echo "<td>$date</td>";
-    //     if ($user_type == "admin"){
-    //         echo"<td>$user_type</td>";
-    //     } else {
-    //         echo "<td>not admin</td>";
-    //     }
-    //     echo "</tr>";
-    // }
+        echo "<tr>";
+        echo "<td>$email</td>";
+        echo "<td>$first $last</td>";
+        echo "<td>$time</td>";
+        echo "<td>$date</td>";
+        if ($user_type == "admin"){
+            echo"<td>$user_type</td>";
+        } else {
+            echo "<td>not admin</td>";
+        }
+        echo "</tr>";
+    }
 }
